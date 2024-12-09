b490dbc59b9a6f168320b0f44df5ab7643e83c96
prasanna1221 <prasannaravi36@gmail.com>
2024-12-09 07:18:31 +0530

    modified

--- Codes/SimpleCode.js
+++ Codes/SimpleCode.js
@@ -1,3 +1,14 @@
 let tex = "AAA";
 const a = 10;
 console.log(tex + a)
+
+
+let employee= {
+    name: "kumar",
+    age:30,
+    details: function(){
+        return this.name+this.age;
+    }
+};
+
+employee.details();
